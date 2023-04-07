dabe8d187f7bfa62a493f7407a00be360d5b5787
htonioni <htonioni@outlook.com>
2023-04-07 14:58:41 -0300

    Added exercise #4

---
+++ Python_Brasil/ex#4.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite a sua primeira nota: "))
+n2 = float(input("Digite a sua segunda nota: "))
+n3 = float(input("Digite a sua terceira nota: "))
+n4 = float(input("Digite a sua quarta nota: "))
+media = (n1+n2+n3+n4)/4
+print(f"A media das suas nota eh: {media}")
