c0163cd27fe0149a322b2544a4ea8e57da9fe0ba
htonioni <htonioni@outlook.com>
2023-04-07 11:58:32 -0300

    Update ex #84

--- Curso_em_video/ex#84.py
+++ Curso_em_video/ex#84.py
@@ -6,18 +6,20 @@ lista_pesos = list()
 menos_pesadas = list()
 mais_pesadas = list()
 
-totp = 0
+# totp = 0 -> substituido pel len()
+
 while True:
     secundaria.append(str(input('Digite seu nome: ')))
-    secundaria.append(int(input('Digite seu peso em kg: ')))
+    secundaria.append(float(input('Digite seu peso em kg: ')))
+    # Criada uma lista de pesos, posteriormente utilzamos max() e min()
     lista_pesos.append(secundaria[1])
     pessoas.append(secundaria[:])
     secundaria.clear()
-    totp += 1 
     continuar = str(input('Gostaria de continuar? [S/N]: '))
     if continuar in ('Nn'):
         break
 
+# Quais as pessoas com menor peso e mair peso
 for c in pessoas:
     if max(lista_pesos) == c[1]:
         mais_pesadas.append(c[0])
@@ -25,6 +27,6 @@ for c in pessoas:
         menos_pesadas.append(c[0])
 
 print('-=' * 45)
-print(f'Voce cadastrou {totp} pessoas!')
+print(f'Voce cadastrou {len(pessoas)} pessoas!')
 print(f'O maior peso foi {max(lista_pesos)}Kg. Peso de: {mais_pesadas}')
 print(f'O menor peso foi {min(lista_pesos)}Kg. Pesos de: {menos_pesadas}')
