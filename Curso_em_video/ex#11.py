bda5536970c5ff8fc0242c2ee8603a0949692b7d
htonioni <htonioni@outlook.com>
2023-04-08 21:40:45 -0300

    Added exercise #11

---
+++ Curso_em_video/ex#11.py
@@ -0,0 +1,5 @@
+largura = float(input('Qual a largura da parede?: '))
+altura = float(input('Qual a altura da sua parede?: '))
+area = largura * altura
+print(f'A sua área é de {area}m²')
+print(f'Para pintar essa parede será necessário {area/2}l de tinta')
