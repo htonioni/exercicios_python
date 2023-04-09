5cd149d0c44359c88f1290ee6e80c67b9df61e6a
htonioni <htonioni@outlook.com>
2023-04-08 21:40:35 -0300

    Added exercise #7

---
+++ Curso_em_video/ex#7.py
@@ -0,0 +1,4 @@
+aluno1 = float(input('Qual sua nota?: '))
+aluno_2 = float(input('Qual sua outra nota?: '))
+med = (aluno1 + aluno_2)/2
+print(f'As notas {aluno1} e {aluno_2} resultam em uma média de <{med}>')
