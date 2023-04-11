4e92a6998be628f9f210ddeaf5910982998fb173
htonioni <htonioni@outlook.com>
2023-04-11 09:18:18 -0300

    Added exercise #27

---
+++ Curso_em_video/ex#27.py
@@ -0,0 +1,5 @@
+nm1 = str(input('Qual seu nome completo?: '))
+nm2 = nm1.split()
+print(f'Seu nome em modo lista é: {nm2}\n'
+        f'Logo seu primeiro nome é: \n{nm2[0]}\n'
+        f'O seu ultimo nome é \n{nm2[-1]}')
