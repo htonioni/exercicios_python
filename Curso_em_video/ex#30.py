ed04edaeaafa9c06e2773b46fda0d1974c4a77df
htonioni <htonioni@outlook.com>
2023-04-15 15:13:32 -0300

    Added exercise #30

---
+++ Curso_em_video/ex#30.py
@@ -0,0 +1,7 @@
+inteiro = int(input('Digite um numero \033[4minteiro\033[m: '))
+if inteiro % 2 == 0:
+    print(f'Seu número {inteiro} é um número \033[4;34mPAR')
+else:
+    print(f'O número {inteiro} é um número \033[4;34IMPAR')
+
+
